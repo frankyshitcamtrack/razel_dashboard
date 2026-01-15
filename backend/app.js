7111ab993804006ff1646f54562dc5e87494d849
KAMSU ULRICH PEROLD <kamsuperold2004@gmail.com>
2026-01-15 09:35:26 +0100

    update dashbaord view

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 24 * 60 * 60 * 1000
   }
 }))
